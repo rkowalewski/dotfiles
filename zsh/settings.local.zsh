ea6aac69fac3f1157e7f44376539e29a7676fd4e
Roger Kowalewski <roger.kowalewski@gmail.com>
2017-03-24 01:25:31 +0100

    remove custom git prompt

--- zsh/settings.local.zsh
+++ zsh/settings.local.zsh
@@ -1,6 +1,1 @@
 #faster git prompt than the default one
-echo "hello we are loaded"
-function git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "${ZSH_THEME_GIT_PROMPT_PREFIX}${ref#refs/heads/}${ZSH_THEME_GIT_PROMPT_CLEAN}${ZSH_THEME_GIT_PROMPT_SUFFIX}"
-}
