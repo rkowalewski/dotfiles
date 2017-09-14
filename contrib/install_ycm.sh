45ec840d881ee68283f6a6ed350db2da248c209d
Roger Kowalewski <roger.kowalewski@gmail.com>
2017-09-14 17:07:28 +0200

    build YCM script

---
+++ contrib/install_ycm.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# For instructions, see http://valloric.github.io/YouCompleteMe/#full-installation-guide
+# We assume here that you have your own LLVM build from source (version >= 3.9)
+
+if ! type clang > /dev/null 2>&1;
+then
+  echo 'clang is not installed or in $PATH';
+fi
+
+CLANG_VERSION="$(clang --version | head -n 1 | sed 's/\(^.*\)\([0-9]\.[0-9]\.[0-9]\)\(.*\)/\2/' | sed 's/\.//g')"
+
+if [ ! $CLANG_VERSION -ge 400 ];
+then
+  echo "ycm requires clang >= 4.0.0"
+  exit 1
+fi
+
+# Update all
+cd ~/.vim/plugged/YouCompleteMe
+git submodule update --init --recursive
+
+BUILD_DIR="$(mktemp -d /tmp/build_ycm.XXXX)"
+
+CLANG_LIB="$(dirname $(which clang))/../lib/libclang.so"
+
+# Generate Makefiles
+cd $BUILD_DIR
+echo "Generating Makefiles..."
+cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=$CLANG_LIB . ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp
+echo "Makefiles have been written to $BUILD_DIR."
+
+# Build YCM
+echo "Building ycm..."
+cmake --build . --target ycm_core --config Release
+echo "Done..."
+
+rm -rf "$BUILD_DIR"
+
