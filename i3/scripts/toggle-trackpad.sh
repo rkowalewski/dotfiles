f75f8b469b016b9303f64283359214a785cbed98
Roger Kowalewski <roger.kowalewski@gmail.com>
2018-11-21 10:08:30 +0100

    addded i3 and i3blocks config

---
+++ i3/scripts/toggle-trackpad.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# xinput list gives you the correct id for the trackpad
+trackpadId="$(xinput list | grep "Synaptics TouchPad" | sed 's/\(.*id=\)\([0-9]\+\)\s.*/\2/')"
+# find out the state
+state="$(xinput list-props $trackpadId | grep 'Device Enabled' | awk 'BEGIN{FS=":"}{print $2}' | xargs)"
+
+if [ "$?" != "0" ]
+then
+    i3-nagbar -m 'xinput command failed' -t warning
+elif [ "$state" = "0" ]
+then
+    xinput enable "$trackpadId"
+else
+    xinput disable "$trackpadId"
+fi
